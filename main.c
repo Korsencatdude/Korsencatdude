c0cd10aff459d8bb6937fc22b200c0bdb868829f
Korsencatdude <113310697+Korsencatdude@users.noreply.github.com>
2022-09-12 22:43:24 +0200

    Add files via upload
    double list with insert node function

--- main.c
+++ main.c
@@ -1,66 +1,67 @@
-
- /* This code has been written by Korsencatdude, and is a solution the the task 10.3 in the book
-  * "Programming in C" by Stephen G. Kohan
-  *If you by any chance want to use this code, please feel free to do so :)
-  *
-  * P.S this code is a work in progress, variable names and other nickpicks will be changed in the
-  * future. Please contact me if you have any suggestions how i can make it better ;)
-  */
-
-
 #include <stdio.h>
-struct week
+struct doubleList
 {
-    float dayOfWeek;
-    struct week *tomorrow;
-    
+    int value;
+    struct doubleList *next;
+    struct doubleList *previous;
 };
+  // insert node changes both the pointer to the
+  //previous node(*previous) and the next node (*next)
 
-//function inserts a struct between the previous day and the next day
-void insert_day(struct week *middleDay, struct week *previousDay)
+void insertNode(struct doubleList *insert, struct doubleList *before)
 {
-   
-    middleDay -> tomorrow = previousDay -> tomorrow;
-    previousDay -> tomorrow = middleDay;
-    
-    
-    
-    
+    insert -> next = before -> next;
+    before -> next -> previous = insert;
+    before -> next = insert;
+    insert -> previous = before;
+ 
     
 }
 
+
 int main(void)
 {
+    struct doubleList header;
+    struct doubleList list1;
+    struct doubleList list2;
+    struct doubleList random;
+    struct doubleList list3;
+    struct doubleList *ptr;
+    
+    list1.value = 1;
+    list2.value = 2;
+    list3.value = 3;
+    random.value = 45;
     
     
-    struct week header;             //declearing a linked list of type week, with header as a start
-    struct week monday;
-    struct week tuesday;
-    struct week wednesday;
-    struct week madeUpDay;
-    struct week *entryPtr;
-    
-    
-    monday.dayOfWeek = 1;
-    tuesday.dayOfWeek = 2;
-    wednesday.dayOfWeek = 3;
-    madeUpDay.dayOfWeek = 2.5;
-    
-    header.tomorrow = &monday;          //header points to the first struct at any given moment
-    entryPtr = header.tomorrow;
-    monday.tomorrow = &tuesday;
-    tuesday.tomorrow = &wednesday;
-    wednesday.tomorrow = (struct week *) '0'; //end of linked list
-    
-    insert_day(&madeUpDay, &header);
-    entryPtr = header.tomorrow;
+
+    header.next = &list1;
+    ptr = header.next;
+    list1.next = &list2;
+    list1.previous = (struct doubleList *) '0';
+    list2.next = &list3;
+    list2.previous = &list1;
+    list3.next = (struct doubleList *) '0';
+    list3.previous = &list2;
     
     
     
-    while (entryPtr != (struct week *) '0')
+    while (ptr != (struct doubleList *) '0')
     {
-        printf("%.2f \n", entryPtr -> dayOfWeek);
-        entryPtr = entryPtr ->tomorrow;
+        printf("%i \n", ptr -> value);
+        ptr = ptr -> next;
+        
+    }
+    
+    
+    insertNode(&random, &list2);
+    ptr = header.next;
+    
+    while (ptr != (struct doubleList *) '0')
+    {
+        printf("%i \n", ptr -> value);
+        ptr = ptr -> next;
+        
     }
     return 0;
 }
